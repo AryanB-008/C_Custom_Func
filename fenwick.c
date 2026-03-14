91b1e79188d7c50ff62acb53a545049e5f416c30
Aryan Bansal <118984766+AryanB-008@users.noreply.github.com>
2026-03-14 14:15:53 +0530

    fixed fenwick tree implementatioin

--- fenwick.c
+++ fenwick.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 
-int sum(int i,int* t){//function to return the sum [1,i]
+int sum(int i, int* t){// to return sum[1,i], 1 indexed
     int s = 0;
     while(i){
-        s+=t[i];
-        s-=s&-s;
+        s += t[i];
+        i -= i & -i;
     }
+    return s;
 }
 
-int range(int a, int b, int* t){//function to return sum [a,b]
-    return sum(b,t)-sum(a-1,t);
+int range(int a, int b, int* t){// to return sum[a,b], 1 indexed
+    return sum(b, t) - sum(a-1, t);
 }
 
-int update(int i, int k, int* t, int n){//function to update value in the tree by adding the difference to right places
-    while(i<=n){
-        t[i]+=k;
-        i+=i&-i;
+void update(int i, int k, int* t, int n){// to update at i, 1 indexed
+    while(i <= n){
+        t[i] += k;
+        i += i & -i;
     }
 }
 
 int main(void){
-    int n;// number of elements in array
-    scanf('%d',&n);
+    int n;
+    scanf("%d", &n);
     int arr[n];
-    for (int i = 0; i<n; i++) scanf("%d",arr+i);
-    int tree[n+1];// the fenwick tree, 1-indexed
-    tree[1] = arr[0];
-    for (int i = 1; i<n; i++) tree[i+1] = arr[i]+sum(i,tree);// to create the fenwick tree
+    for (int i = 0; i < n; i++) scanf("%d", arr+i);
+    int tree[n+1];
+    for (int i = 0; i <= n; i++) tree[i] = 0;
+    for (int i = 0; i < n; i++) update(i+1, arr[i], tree, n);
 }
